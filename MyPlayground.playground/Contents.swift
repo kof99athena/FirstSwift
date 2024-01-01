73e3eb4b38cbcbba2d582ecfb0c31796fa72dc2c
kof99athena <kof99athena@gmail.com>
2024-01-01 23:32:06 +0900

    if문

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//다크모드 여ㅜ부
+var isDarkMode : Bool = false
+
+if !isDarkMode{
+    print("다크모드 입니다.")
+}else{
+    print("다크모드가 아닙니다.")
+}
+
+var title : String = !isDarkMode ? "다크모드 입니다." : "다크모드가 아닙니다."
+print("title:\(title)")
