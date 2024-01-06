8dd6880999e171532c662065a0c81dcebb64ab8e
kof99athena <kof99athena@gmail.com>
2024-01-06 16:58:03 +0900

    enum

--- FirstSwift/EnumTest.playground/Contents.swift
+++ FirstSwift/EnumTest.playground/Contents.swift
@@ -1,3 +1,46 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//enum : 타입을 나누는 것, 경우를  나누는것
+
+enum School {
+//    case elementary
+//    case middle
+//    case high
+    
+    case elementary, middle, high
+    
+}
+
+//var : 값이 변경 안됨 , let : 값 변경 가능
+let yourSchool = School.high
+print("yourSchool : \(yourSchool)")
+
+enum Grade : Int {
+    //enum은 값을 가질수도있다.
+    case First = 1
+    case Second = 2
+}
+
+let yourGrade = Grade.Second.rawValue //값에 접근
+print("yourGrade : \(yourGrade)")
+
+enum SchoolDatail{
+    case elementary(name : String)
+    case middle(name : String)
+    case high(name : String)
+    
+    func getName() -> String {
+        switch self {
+        case .elementary(let name):
+            return name
+        case .middle(let name):
+            return name
+        case .high(let name):
+            return name
+        }
+        
+    }
+    
+}
+let yourMiddleSchoolName = SchoolDatail.middle(name: "서울중학교")
+print("name : \(yourMiddleSchoolName.getName())")
--- FirstSwift/ForEachTest.playground/Contents.swift
+++ FirstSwift/ForEachTest.playground/Contents.swift
@@ -1,3 +1,43 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//foreach
+//컬렉션 : 데이터를 모아둔것
+//Arrays, Sets, Dictionaries, 튜플
+var myArray : [Int] = [0,1,2,3,4,5,6,7,8,9,10]
+
+for item in myArray where item > 5 {
+    print("5보다 큰수 : \(item)")
+}
+
+for item in myArray where item % 2 == 0 {
+    print("짝수 : \(item)")
+}
+
+for item in myArray where item % 2 != 0 {
+    print("홀수 : \(item)")
+}
+
+//for index in 0...5 {
+//    //0,1,2,3,4,5 총 6개
+//    print("haha index : \(index)")
+//}
+
+//for index in 0...4 {
+//    //0,1,2,3,4,5 총 6개
+//    print("haha index : \(index)")
+//}
+
+for index in 0...4 where index % 2 == 0{
+    //0,1,2,3,4,5 총 6개
+    print("haha index : \(index)")
+}
+
+var randomInts : [Int] = [Int]()
+
+for _ in 0 ..< 25{
+    let randomNumber = Int.random(in: 0...100)
+    randomInts.append(randomNumber)
+}
+
+print("randomInts : \(randomInts)")
+
